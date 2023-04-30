9e6fcbaf87f0e86ea0aeff80c827a80fb307d61b
Kübra Felek <49831711+kubrafelek@users.noreply.github.com>
2023-04-30 18:28:56 +0000

    Reducer file fixed by deleting file

--- frontend/src/reducers/productReducers.js
+++ frontend/src/reducers/productReducers.js
@@ -25,10 +25,6 @@ import {
     PRODUCT_CREATE_REVIEW_SUCCESS,
     PRODUCT_CREATE_REVIEW_FAIL,
     PRODUCT_CREATE_REVIEW_RESET,
-
-    PRODUCT_TOP_REQUEST,
-    PRODUCT_TOP_SUCCESS,
-    PRODUCT_TOP_FAIL,
 } from '../constants/productConstants'
 
 
@@ -123,44 +119,6 @@ export const productUpdateReducer = (state = { product: {} }, action) => {
 
         case PRODUCT_UPDATE_RESET:
             return { product: {} }
-
-        default:
-            return state
-    }
-}
-
-
-
-export const productReviewCreateReducer = (state = {}, action) => {
-    switch (action.type) {
-        case PRODUCT_CREATE_REVIEW_REQUEST:
-            return { loading: true }
-
-        case PRODUCT_CREATE_REVIEW_SUCCESS:
-            return { loading: false, success: true, }
-
-        case PRODUCT_CREATE_REVIEW_FAIL:
-            return { loading: false, error: action.payload }
-
-        case PRODUCT_CREATE_REVIEW_RESET:
-            return {}
-
-        default:
-            return state
-    }
-}
-
-
-export const productTopRatedReducer = (state = { products: [] }, action) => {
-    switch (action.type) {
-        case PRODUCT_TOP_REQUEST:
-            return { loading: true, products: [] }
-
-        case PRODUCT_TOP_SUCCESS:
-            return { loading: false, products: action.payload, }
-
-        case PRODUCT_TOP_FAIL:
-            return { loading: false, error: action.payload }
 
         default:
             return state
